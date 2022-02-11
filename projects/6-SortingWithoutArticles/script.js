e7a90f046b3a52b03595a2386c112f4cdbee48fc
David <david-bitner@hotmail.com>
2022-02-11 10:15:29 -0300

    Update project6
    Small change to unsorted list

--- projects/6-SortingWithoutArticles/script.js
+++ projects/6-SortingWithoutArticles/script.js
@@ -4,11 +4,13 @@ function strip(bandName) {
   return bandName.replace(/^(a |the |an)/i, "").trim();
 }
 
+BANDS.sort();
+
 document.querySelector(".unsorted").innerHTML = BANDS.map(
   (band) => `<li>${band}</li>`
 ).join("");
 
-const sortedBands = BANDS.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+BANDS.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
 
 document.querySelector(".sorted").innerHTML = BANDS.map(
   (band) => `<li>${band}</li>`
