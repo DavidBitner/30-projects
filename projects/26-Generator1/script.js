a3f413c4de66a26771dfc2683ad9c3815bb90710
David <david-bitner@hotmail.com>
2022-03-23 06:24:53 -0300

    test

--- projects/26-Generator1/script.js
+++ projects/26-Generator1/script.js
@@ -13,3 +13,16 @@ async function generateLoremIpsum() {
   console.log(data);
 }
 
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "baconator-bacon-ipsum.p.rapidapi.com",
+    "X-RapidAPI-Key": "bb29e62583msh10c5bfe768c328bp121b43jsn96ae4388c74c",
+  },
+};
+
+fetch("https://baconator-bacon-ipsum.p.rapidapi.com/?type=all-meat", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+  
